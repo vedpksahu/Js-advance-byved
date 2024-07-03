5c2599c55894d2162282b42fb9bd75f98c8e9c07
vedpksahu <95854393+vedpksahu@users.noreply.github.com>
2024-07-03 11:20:49 +0000

    modified

--- .devcontainer/01_basics/conandoperation.js
+++ .devcontainer/01_basics/conandoperation.js
@@ -1,27 +1,72 @@
 let score = false;
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "ved"
+let isLoggedIn = "ved";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // " " => false
 //"ved" => true
 
-let someNumber = 33
+let someNumber = 33;
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log( typeof stringNumber);
+
+// operations//////
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "ved";
+let str2 = "sahu";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+
+//===
 
