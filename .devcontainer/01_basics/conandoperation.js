b4c41e9e857248570e186b840d10789feccbf3c4
vedpksahu <95854393+vedpksahu@users.noreply.github.com>
2024-07-03 09:29:25 +0000

    coversion of datatypes

---
+++ .devcontainer/01_basics/conandoperation.js
@@ -0,0 +1,27 @@
+let score = false;
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "ved"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// " " => false
+//"ved" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
