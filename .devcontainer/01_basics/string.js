d3955cde88704f98a7e2ac5064551567d9249f90
vedpksahu <95854393+vedpksahu@users.noreply.github.com>
2024-07-04 07:01:22 +0000

    modified

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,34 @@
+const name = "ved";
+const repoCount = "29";
+
+// console.log(name + repoCount + "value");
+
+//modern syntax
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Ved-icon");
+
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('c'));
+
+const newString = gameName.substring(0, 6);
+//console.log(newString);
+
+const anotherString = gameName.slice(-6, 8);
+//console.log(anotherString);
+
+const newStringOne = "   vedprakash   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://vedportfolio.com/vedb";
+// console.log(url.replace('.com','.gmail'));
+
+//console.log(url.includes('vedbP'));
+
+console.log(gameName.split('-'));
